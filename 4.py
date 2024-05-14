8fbae1de696d4294750eed793bda7f6422a8c85a
kartiks-git <166221473+kartiks-git@users.noreply.github.com>
2024-05-15 01:22:04 +0530

    re-uploading

---
+++ 4.py
@@ -0,0 +1,7 @@
+'''
+The provided code stub reads and integer, , from STDIN. For all non-negative integers , print n square.
+'''
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (0,n):
+        print(i*i)
